2858337a014cda6d47a5e65969b092d75aa765a2
Albin Wärneryd <al3765ho-s@student.lu.se>
2026-02-19 20:06:21 +0100

    hardened blazor server circuit settings for reconnect stability

--- src/Normyx.Web/Program.cs
+++ src/Normyx.Web/Program.cs
@@ -4,7 +4,14 @@ using Normyx.Web.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+    .AddInteractiveServerComponents(options =>
+    {
+        options.DetailedErrors = builder.Environment.IsDevelopment();
+        options.DisconnectedCircuitRetentionPeriod = TimeSpan.FromMinutes(5);
+        options.DisconnectedCircuitMaxRetained = 200;
+        options.JSInteropDefaultCallTimeout = TimeSpan.FromSeconds(20);
+        options.MaxBufferedUnacknowledgedRenderBatches = 20;
+    });
 
 var apiBase = builder.Configuration["ApiBaseUrl"] ?? "http://localhost:8080";
 builder.Services.AddHttpClient("NormyxApi", client =>
